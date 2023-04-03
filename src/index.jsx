6670d71550748610b2b2013c2183dd47f28ea364
Juliano Sieler <sieler@amicaldo.de>
2023-04-03 13:47:59 +0200

    First Try at webhooks

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
-import { webTrigger } from "@forge/api";
-import ForgeUI, { render, AdminPage, Form, Fragment, Heading, Text, TextField, useState } from '@forge/ui';
+import xml from "xml";
+import api, { route, webTrigger } from "@forge/api";
+import ForgeUI, { render, AdminPage, Form, Fragment, Heading, Text, TextField, useState, Row } from "@forge/ui";
 
 const App = () => {
     const [webTriggerURL, setWebTriggerURL] = useState("")
     const onSubmit = async (formData) => {
         var url = await webTrigger.getUrl("sipgateCall")
 
-        url += `?project=${formData.projectID}&phoneField=${formData.cField1}&handyField=${formData.cField2}`
+        url += `?project=${formData.projectID}&handyField=${formData.cField1}&phoneField=${formData.cField2}&issueID=${formData.issueID}`
 
         setWebTriggerURL(url)
     }
@@ -18,13 +19,65 @@ const App = () => {
                 <TextField name="projectID" isRequired type="text" description="ID of the project" />
                 <TextField name="cField1" isRequired type="number" description="ID of the first Custom Field" />
                 <TextField name="cField2" isRequired type="number" description="ID of the second Custom Field" />
+                <TextField name="issueID" isRequired type="number" description="ID of the issueType" />
             </Form>
             <Text>{webTriggerURL}</Text>
         </Fragment>
     );
 };
 
-export const SipgateCall = () => {
+export async function SipgateCall(req) {
+    try {
+        const body = JSON.parse(req.body)
+        const queryParameters = body.queryParameters
+        var answerURL = await webTrigger.getUrl("sipgateAnswer")
+        var hangupURL = await webTrigger.getUrl("sipgateHangup")
+
+        const response = await api.asUser().requestJira(route`/rest/api/3/issue`, {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: `{
+                summary: "Call from ${body.from}",
+                issuetype: {
+                    id: "${queryParameters.issueID}"
+                },
+                project: {
+                    key: "${queryParameters.projectID}"
+                },
+                customfield_${queryParameters.handyField}: "0",
+                customfield_${queryParameters.phoneField}: "${body.from}"
+            }`
+        })
+
+        return {
+            headers: { "Content-Type": ["application/json"] },
+            body: xml({
+                Response: [
+                    { _attr: { onAnswer: answerURL } },
+                    { _attr: { onHangup: hangupURL } }
+                ]
+            }),
+            statusCode: 200,
+            statusText: "OK"
+        }
+    } catch (error) {
+        return {
+            body: error + "\n",
+            headers: { "Content-Type": ["application/json"] },
+            statusCode: 400,
+            statusText: "Bad Request",
+        }
+    }
+}
+
+export async function SipgateAnswer(req) {
+
+}
+
+export async function SipgateHangup(req) {
 
 }
 
