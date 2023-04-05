b0c7290335aa402eb38375d779c06e608c2d6ca5
Juliano Sieler <sieler@amicaldo.de>
2023-04-05 11:42:47 +0200

    Added Summary edit; Added summary formatting; added tellows

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
-import xml from "xml";
-import api, { authorize, route, startsWith, storage, webTrigger } from "@forge/api";
-import ForgeUI, { render, AdminPage, Form, Fragment, Heading, Text, TextField, useState, useEffect, User } from "@forge/ui";
+import xml from "xml"
+import dayjs from "dayjs"
+import api, { fetch, route, startsWith, storage, webTrigger } from "@forge/api"
+import ForgeUI, { render, AdminPage, Form, Fragment, Heading, Text, TextField, useState, useEffect, User } from "@forge/ui"
 
 function getBodyData(body) {
     let obj = {}
@@ -17,6 +18,14 @@ function getBodyData(body) {
 function App() {
     const [webTriggerURL, setWebTriggerURL] = useState("")
     const [users, setUsers] = useState([])
+    const [issueConfig, setIssueConfig] = useState([])
+    const issueSubmit = formData => {
+        storage.set("issueSummary", formData.issueSummary)
+        storage.set("spamRatingField", formData.spamRatingField)
+        storage.set("cityField", formData.cityField)
+
+        setIssueConfig([formData.issueSummary, formData.spamRatingField, formData.cityField])
+    }
     const generatorSubmit = async formData => {
         var url = await webTrigger.getUrl("sipgateCall")
 
@@ -24,7 +33,7 @@ function App() {
 
         setWebTriggerURL(url)
     }
-    const userSubmit = async formData => {
+    const userSubmit = formData => {
         let usersCopy = [...users]
 
         for (const [atlassianID, sipgateID] of Object.entries(formData)) {
@@ -32,7 +41,7 @@ function App() {
                 const dataIndex = usersCopy.findIndex(user => user.accountId === atlassianID)
 
                 if (dataIndex !== -1) {
-                    usersCopy[dataIndex] = {...usersCopy[dataIndex], sipgateID}
+                    usersCopy[dataIndex] = { ...usersCopy[dataIndex], sipgateID }
 
                     storage.set(`sipgate_id_${sipgateID}`, atlassianID)
                 }
@@ -47,6 +56,9 @@ function App() {
         const usersData = await usersRaw.json()
         const usersFiltered = usersData.filter(user => user.accountType !== "app" && user.displayName !== "Former user")
         const dataLength = usersFiltered.length > 20 ? Math.ceil(usersFiltered.length / 20) : 1
+        const issueSummary = await storage.get("issueSummary")
+        const spamRatingField = await storage.get("spamRatingField")
+        const cityField = await storage.get("cityField")
         let storageData = []
         let usersArr = []
         let cursor = ""
@@ -58,18 +70,17 @@ function App() {
             cursor = storageDataPull.nextCursor
         }
 
-        usersFiltered.forEach(({ accountId, displayName }) => {
+        usersFiltered.forEach(({ accountId }) => {
             usersArr.push({
-                accountId,
-                displayName
+                accountId
             })
         })
 
-        storageData.forEach(async ({key: sipgateID, value: accountId}) => {
+        storageData.forEach(async ({ key: sipgateID, value: accountId }) => {
             const dataIndex = usersArr.findIndex(user => user.accountId === accountId)
 
             if (dataIndex !== -1) {
-                usersArr[dataIndex] = {...usersArr[dataIndex], sipgateID: sipgateID.replace("sipgate_id_", "")}
+                usersArr[dataIndex] = { ...usersArr[dataIndex], sipgateID: sipgateID.replace("sipgate_id_", "") }
             }
             else {
                 await storage.delete(sipgateID)
@@ -77,10 +88,21 @@ function App() {
         })
 
         setUsers(usersArr)
+        setIssueConfig([
+            issueSummary ? issueSummary : "Anruf von {{$numberOrName}}{{$spamRatingField}}{{$cityField}} - {{$date}} - {{$time}} Uhr",
+            spamRatingField ? spamRatingField : " (Rate: {{$rating}})",
+            cityField ? cityField : " aus {{$city}}"
+        ])
     }, [])
 
     return (
         <Fragment>
+            <Heading>Issue Configuration</Heading>
+            <Form onSubmit={issueSubmit}>
+                <TextField name="issueSummary" type="text" isRequired defaultValue={issueConfig[0]} description="" />
+                <TextField name="spamRatingField" type="text" isRequired defaultValue={issueConfig[1]} />
+                <TextField name="cityField" type="text" isRequired defaultValue={issueConfig[2]} />
+            </Form>
             <Heading>Sipgate Webhook Generator</Heading>
             <Form onSubmit={generatorSubmit}>
                 <TextField name="projectID" isRequired type="text" description="ID of the project" />
@@ -109,15 +131,26 @@ function App() {
                 </Fragment>
             )}
         </Fragment>
-    );
-};
+    )
+}
 
 export async function SipgateCall(req) {
     try {
         const body = getBodyData(req.body)
         const queryParameters = req.queryParameters
-        var answerURL = await webTrigger.getUrl("sipgateAnswer")
-        var hangupURL = await webTrigger.getUrl("sipgateHangup")
+        const answerURL = await webTrigger.getUrl("sipgateAnswer")
+        const hangupURL = await webTrigger.getUrl("sipgateHangup")
+        const tellowsRaw = await fetch(`https://www.tellows.de/basic/num/${body.from}?json=1`)
+        const tellows = await tellowsRaw.json()
+        const issueSummary = await storage.get("issueSummary")
+        const spamRatingField = await storage.get("spamRatingField")
+        const cityField = await storage.get("cityField")
+        let summary = `${issueSummary}`
+            .replace("{{$numberOrName}}", `+${body.from}`)
+            .replace("{{$spamRatingField}}", tellows?.tellows?.score ? `${spamRatingField}`.replace("{{$rating}}", tellows.tellows.score) : "")
+            .replace("{{$cityField}}", tellows?.tellows?.location ? `${cityField}`.replace("{{$city}}", tellows.tellows.location) : "")
+            .replace("{{$date}}", dayjs().add(2, "hour").format("DD.MM.YY"))
+            .replace("{{$time}}", dayjs().add(2, "hour").format("HH:mm"))
 
         const issueRaw = await api.asApp().requestJira(route`/rest/api/3/issue`, {
             method: "POST",
@@ -127,21 +160,19 @@ export async function SipgateCall(req) {
             },
             body: JSON.stringify({
                 fields: {
-                    summary: `Call from ${body.from}`,
+                    summary,
                     issuetype: {
                         id: queryParameters.issueID[0]
                     },
                     project: {
                         key: queryParameters.project[0]
                     },
-                    [`customfield_${queryParameters.handyField[0]}`]: "0",
+                    [`customfield_${queryParameters.handyField[0]}`]: "",
                     [`customfield_${queryParameters.phoneField[0]}`]: body.from
                 }
             })
         })
         const issue = await issueRaw.json()
-
-        console.log(issue)
 
         return {
             headers: { "Content-Type": ["application/json"] },
@@ -166,48 +197,18 @@ export async function SipgateCall(req) {
 
 export async function SipgateAnswer(req) {
     const queryParameters = req.queryParameters
-    let canEdit
-    let canAssign
 
-    try {
-        console.log("Check Auth")
-
-        canEdit = await authorize().onJiraIssue(queryParameters.issueID[0]).canEditIssues()
-        canAssign = await authorize().onJiraIssue(queryParameters.issueID[0]).canAssign()
-
-        console.log("After Auth Check")
-    } catch (error) {
-        console.error(error)
-    }
-
-    if (canEdit && canAssign) {
-        console.log("Can Edit")
-
-        const response = await api.asApp().requestJira(route`/rest`)
-    }
+    const response = await api.asApp().requestJira(route`/rest`)
 }
 
 export async function SipgateHangup(req) {
     const queryParameters = req.queryParameters
-    let canEdit
 
-    try {
-        console.log("Check Auth")
-
-        canEdit = await authorize().onJiraIssue(queryParameters.issueID[0]).canEditIssues()
-
-        console.log("After Auth Check")
-    } catch (error) {
-        console.error(error)
-    }
-
-    if (canEdit) {
-        const response = await api.asApp().requestJira(route`/rest`)
-    }
+    const response = await api.asApp().requestJira(route`/rest`)
 }
 
 export const run = render(
     <AdminPage>
         <App />
     </AdminPage>
-);
+)
