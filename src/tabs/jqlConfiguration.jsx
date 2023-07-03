bdd1827acdeedd162ed493d13bdd90e1d1dc4c24
Juliano Sieler <sieler@amicaldo.de>
2023-07-03 13:23:28 +0200

    Updated the JQL Query Tab to include a defaultValue

--- src/tabs/jqlConfiguration.jsx
+++ src/tabs/jqlConfiguration.jsx
@@ -1,5 +1,18 @@
 import { storage } from "@forge/api";
-import ForgeUI, { useEffect, useState, Form, Fragment, SectionMessage, Tab, Text, TextField, Toggle } from "@forge/ui";
+import ForgeUI, { useEffect, useState, Form, FormCondition, Fragment, Option, SectionMessage, Select, Tab, Text, TextField, Toggle } from "@forge/ui";
+
+const options = [
+    "Custom Text",
+    "{{$number}}",
+    "{{$date}}",
+    "{{$time}}",
+    "{{$rating}}",
+    "{{$city}}",
+    "{{$sipgateNumber}}",
+    "{{$spamRatingField}}",
+    "{{$cityField}}",
+    "{{$timeField}}"
+]
 
 export default function JQLConfiguration() {
     const [jql, setJQL] = useState({})
@@ -15,7 +28,7 @@ export default function JQLConfiguration() {
 
         if (queriesLength < formData.jqlQueriesAmount) {
             for (let i = 0; i < formData.jqlQueriesAmount - queriesLength; i++) {
-                queries.push({ query: "", variable: "" })
+                queries.push({ query: "", variable: "", defaultValue: "{{$time}}" })
             }
         }
 
@@ -36,7 +49,11 @@ export default function JQLConfiguration() {
         var queries = []
 
         for (let i = 0; i < jql.queriesAmount; i++) {
-            queries.push({ query: formData[`query_${i}`], variable: formData[`variable_${i}`]})
+            queries.push({
+                query: formData[`query_${i}`],
+                variable: formData[`variable_${i}`],
+                defaultValue: formData[`select_${i}`] === "Custom Text" ? formData[`custom_${i}`] : formData[`select_${i}`]
+            })
         }
 
         setJQL({
@@ -68,15 +85,53 @@ export default function JQLConfiguration() {
                 </Text>
             </SectionMessage>
             <Form onSubmit={submitForm}>
-                <Toggle label="Enable JQL" name="jqlEnable" defaultChecked={jql.enabled} />
-                <TextField isRequired type="number" label="JQL Queries" name="jqlQueriesAmount" defaultValue={jql.queriesAmount} />
+                <Toggle
+                    label="Enable JQL"
+                    name="jqlEnable"
+                    defaultChecked={jql.enabled}
+                />
+                <TextField
+                    isRequired
+                    type="number"
+                    label="JQL Queries"
+                    name="jqlQueriesAmount"
+                    defaultValue={jql.queriesAmount}
+                />
             </Form>
             {jql?.queries && jql.queries.length > 0 && (
                 <Form onSubmit={submitJQL}>
-                    {jql.queries.map(({ query, variable }, index) => (
+                    {jql.queries.map(({ query, variable, defaultValue }, index) => (
                         <Fragment>
-                            <TextField name={`query_${index}`} label={`JQL Query ${`${index + 1}`.padStart(2, "0")}`} defaultValue={query} />
-                            <TextField name={`variable_${index}`} label={`JQL Variable ${`${index + 1}`.padStart(2, "0")}`} defaultValue={variable} />
+                            <TextField
+                                name={`query_${index}`}
+                                label={`JQL Query ${`${index + 1}`.padStart(2, "0")}`}
+                                defaultValue={query} />
+                            <TextField
+                                name={`variable_${index}`}
+                                label={`JQL Variable ${`${index + 1}`.padStart(2, "0")}`}
+                                defaultValue={variable}
+                            />
+                            <Select
+                                name={`select_${index}`}
+                                label={`Default Value ${`${index + 1}`.padStart(2, "0")}`}
+                            >
+                                {options.map((option, index) => (
+                                    <Option
+                                        label={option}
+                                        defaultSelected={
+                                            index === 0 && !options.includes(defaultValue) ?
+                                                true : option === defaultValue
+                                        }
+                                        value={option} />
+                                ))}
+                            </Select>
+                            <FormCondition when={`select_${index}`} is={"Custom Text"}>
+                                <TextField
+                                    label={`Custom Text ${`${index + 1}`.padStart(2, "0")}`}
+                                    name={`custom_${index}`}
+                                    defaultValue={options.includes(defaultValue) ? "" : defaultValue}
+                                />
+                            </FormCondition>
                         </Fragment>
                     ))}
                 </Form>
