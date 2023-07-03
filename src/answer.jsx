2a891b097a0a0a831a24342552e7de3d76b3b1de
Juliano Sieler <sieler@amicaldo.de>
2023-07-03 15:34:00 +0200

    Refactor answer.jsx to work with the new Manager's

--- src/answer.jsx
+++ src/answer.jsx
@@ -3,7 +3,9 @@ import utc from "dayjs/plugin/utc"
 import timezone from "dayjs/plugin/timezone"
 import api, { route, storage } from "@forge/api"
 import getBodyData from "./lib/getBodyData"
-import debugLogging from "./lib/debugLogging"
+import JIRAManager from "./lib/JIRAManager"
+import ReplacementManager from "./lib/ReplacementManager"
+import DebugManager from "./lib/debugManager"
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
@@ -11,83 +13,59 @@ dayjs.extend(timezone)
 export async function SipgateAnswer(req) {
     const issueConfiguration = await storage.get("issueConfiguration")
     const debug = await storage.get("debug")
-    const debugOption = debug.debugOption
-    const debugLog = debug.debugLog
-    const dateData = dayjs().tz(issueConfiguration.timezone)
-    const time = dateData.format(issueConfiguration.hourFormat)
-    const timeField = issueConfiguration.timeField.replace("{{$time}}", time)
+    const callAnsweredDate = dayjs().tz(issueConfiguration.timezone)
+
+    const jiraManager = new JIRAManager()
+    const replacementManager = new ReplacementManager()
+    const debugManager = new DebugManager()
+
+    const time = callAnsweredDate.format(issueConfiguration.hourFormat)
+    const timeField = ReplacementManager.replaceVariables(issueConfiguration.timeField, [
+        ["{{$time}}", time]
+    ]);
 
     try {
         const body = getBodyData(req.body)
 
-        debugLogging(debugOption, debugLog, [
+        debugManager.log(debug, [
             `${timeField}: SipgateAnswer Func -> Answering Call`,
             `${timeField}: SipgateAnswer Func -> Body Data: ${JSON.stringify(body, null, 4)}`
         ])
 
         if (body.direction === "in") {
-            const data = await storage.get(body.xcid)
+            const callInfoFromStorage = await storage.get(body.xcid)
             const userID = body.userId ? body.userId : body["userId%5B%5D"] ? body["userId%5B%5D"] : ""
             const user = body.user ? body.user : body["user%5B%5D"] ? body["user%5B%5D"] : ""
             const accountId = await storage.get(`sipgate_id_${Array.isArray(userID) ? userID[0] : userID}`)
 
-            if (data) {
-                const callDuration = dateData.diff(dayjs(data.date), "s")
+            if (callInfoFromStorage) {
                 const callLogConfiguration = await storage.get("callLogConfiguration")
-                const description = `${callLogConfiguration?.answerCall ? `\n${callLogConfiguration.answerCall}` : ""}`
-                    .replace("{{$timeField}}", issueConfiguration.timeField)
-                    .replace("{{$number}}", `#${body.from}`)
-                    .replace("{{$date}}", dateData.format(issueConfiguration.dateFormat))
-                    .replace("{{$sipgateNumber}}", body.to.replace(issueConfiguration.sipgateNumber, ""))
-                    .replace("{{$sipgateUsername}}", user.replace("+", " "))
-                    .replace("{{$sipgatePassword}}", body.userId ? body.userId : body["userId%5B%5D"] ? body["userId%5B%5D"] : "")
-                    .replace("{{$time}}", time)
-                    .replace("{{$minutes}}", `${Math.floor(callDuration / 60)}`.padStart(2, "0"))
-                    .replace("{{$seconds}}", `${callDuration % 60}`.padStart(2, "0"))
-
-                const resDes = await api.asApp().requestJira(route`/rest/api/3/issue/${data.id}`, {
-                    method: "PUT",
-                    headers: {
-                        "Accept": "application/json",
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({
-                        fields: {
-                            description: {
-                                content: [{
-                                    content: [{
-                                        text: `${data.description}${description}`,
-                                        type: "text"
-                                    }],
-                                    type: "paragraph"
-                                }],
-                                type: "doc",
-                                version: 1
-                            }
-                        }
-                    })
+                const replacements = replacementManager.createReplacementMapping({
+                    issueConfiguration,
+                    body,
+                    callActionDate: callAnsweredDate,
+                    callInfoFromStorage
                 })
+                var description = `${callInfoFromStorage.description}${callLogConfiguration?.answerCall ? `\n${callLogConfiguration.answerCall}` : ""}`
 
-                debugLogging(debugOption, debugLog, [
+                description = ReplacementManager.replaceJQLVariables(description, replacements)
+                description = ReplacementManager.replaceVariables(description, replacements)
+
+                const resDes = jiraManager.updateIssueDescription(callInfoFromStorage.id, description)
+
+                debugManager.log(debug, [
                     `${timeField}: SipgateAnswer Func -> UserID: ${JSON.stringify(userID, null, 4)}`,
                     `${timeField}: SipgateAnswer Func -> User: ${JSON.stringify(user, null, 4)}`,
                     `${timeField}: SipgateAnswer Func -> Edited Issue Response: ${JSON.stringify(resDes, null, 4)}`,
                     `${timeField}: SipgateAnswer Func -> Edited Description: ${data.description}${description}`
                 ])
 
-                await storage.set(body.xcid, { ...data, description: `${data.description}${description}`, date: dateData.toJSON() })
+                await storage.set(body.xcid, { ...data, description: `${data.description}${description}`, date: callAnsweredDate.toJSON() })
 
                 if (accountId) {
-                    const resAs = await api.asApp().requestJira(route`/rest/api/3/issue/${data.id}/assignee`, {
-                        method: "PUT",
-                        headers: {
-                            "Accept": "application/json",
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({ accountId })
-                    })
+                    const resAs = jiraManager.assignUser(data.id, accountId)
 
-                    debugLogging(debugOption, debugLog, [
+                    debugManager.log(debug, [
                         `${timeField}: SipgateAnswer Func -> Assign Response: ${JSON.stringify(resAs, null, 4)}`
                     ])
                 }
@@ -101,12 +79,12 @@ export async function SipgateAnswer(req) {
             }
         }
     } catch (error) {
-        if (debugOption) {
-            debugLog.push(`${timeField}: SipgateAnswer Func -> Error: ${JSON.stringify(error, null, 4)}`)
+        if (debug.debugOption) {
+            debug.debugLog.push(`${timeField}: SipgateAnswer Func -> Error: ${JSON.stringify(error, null, 4)}`)
 
             console.error(`${timeField}: SipgateAnswer Func -> Error: ${JSON.stringify(error, null, 4)}`)
 
-            await storage.set("debug", { debugOption, debugLog })
+            await storage.set("debug", debug)
         }
 
         return {
