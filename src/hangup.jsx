8b441a2797c5be7803b6828cfdb909849ce691e7
Juliano Sieler <sieler@amicaldo.de>
2023-07-04 13:20:18 +0200

    Removed not needed import's

--- src/hangup.jsx
+++ src/hangup.jsx
@@ -1,7 +1,7 @@
 import dayjs from "dayjs"
 import utc from "dayjs/plugin/utc"
 import timezone from "dayjs/plugin/timezone"
-import api, { route, storage } from "@forge/api"
+import { storage } from "@forge/api"
 import getBodyData from "./lib/getBodyData"
 import JIRAManager from "./lib/JIRAManager"
 import ReplacementManager from "./lib/ReplacementManager"
